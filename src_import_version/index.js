5196e935599fc0c1836972571537aa14ecf4d260
theajack <theajack@qq.com>
2019-09-29 08:26:45 +0800

    fix: 删除无用的文件夹

--- src_import_version/index.js
+++
@@ -1,9 +0,0 @@
-// import cnchar from './main/index'
-// import './plugin/order'
-// import './plugin/trad'
-// import './plugin/poly'
-// console.log('1',cnchar)
-// export default cnchar;
-import b from './b'
-// var b = require('./b')
-console.log(b);
